2bc3ee9756b5cf7418b362f1fe8841c684a16e9c
Luciana Brancato <luciana.brancato@gmail.com>
2024-05-30 10:33:38 +0200

    Installation de vuejs3 et vite

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TestComponent from './components/TestComponent.vue';
+import { onMounted } from 'vue';
+
+    onMounted(() => console.log('Hello World'));
+</script>
+
+<template>
+    <h1>Hello World</h1>
+    <TestComponent />
+</template>
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,3 @@
+<template>
+    Test123
+</template>
